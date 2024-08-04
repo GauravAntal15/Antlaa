75e7eed988547f6f6af83961b4f31a55e84d9fbe
Gaurav Antal <68815799+GauravAntal15@users.noreply.github.com>
2024-08-04 20:32:35 +0530

    Changing the content of the grid.

--- data/index.ts
+++ data/index.ts
@@ -38,7 +38,7 @@ export const gridItems = [
   },
   {
     id: 4,
-    title: "Tech enthusiast with a passion for development.",
+    title: "Working as a Data Analyst at Consilio LLC.",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
@@ -49,7 +49,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building a JS Animation library",
+    title: "I'm Currently Learning AWS, Kubernetes, DevOps, Docker",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
