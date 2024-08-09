d381bb0c1fcdbb01e075f2c322cda510a7958441
Gaurav Antal <68815799+GauravAntal15@users.noreply.github.com>
2024-08-09 12:22:24 +0530

    changing number of projects.

--- data/index.ts
+++ data/index.ts
@@ -80,7 +80,7 @@ export const projects = [
     iconLists: ["/py.svg", "/and.svg", "/java.svg"],
     link: "https://github.com/GauravAntal15",
   },
-  {
+  /*{
     id: 2,
     title: "Yoom - Video Conferencing App",
     des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
@@ -96,13 +96,15 @@ export const projects = [
     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
     link: "/ui.aiimg.com",
   },
+  */
   {
     id: 4,
     title: "Animated Apple Iphone 3D Website",
     des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
     img: "/p4.svg",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    link: "/ui.apple.com",
+    link: "https://github.com/GauravAntal15",
+
   },
 ];
 
