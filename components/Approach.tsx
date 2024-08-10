ad8a6e3304847395794cf1c90e53597cbf0ad54e
Gaurav Antal <68815799+GauravAntal15@users.noreply.github.com>
2024-08-10 20:20:50 +0530

    Update

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -148,7 +148,7 @@ const AceternityIcon = ({ order }: { order: string }) => {
       {/* remove text-sm font-medium h-12 , add font-bold text-2xl */}
       <button className="relative inline-flex h-12 w-full md:w-60 md:mt-10 overflow-hidden rounded-lg p-[1px] focus:outline-none ">
         <span
-          className="absolute inset-[-1000%]  bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)] rounded-lg"
+          className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)] rounded-lg"
         />
         <span
           className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg
