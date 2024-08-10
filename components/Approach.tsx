76de6d46de437ceba944145732fc21670c2616c7
Gaurav Antal <68815799+GauravAntal15@users.noreply.github.com>
2024-08-10 20:19:26 +0530

    Final Approach content

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -16,9 +16,7 @@ const Approach = () => {
         <Card
           title="Planning & Strategy"
           icon={<AceternityIcon order="Phase 1" />}
-          des="We'll collaborate to map out your website's goals, target audience, 
-          and key functionalities. We'll discuss things like site structure, 
-          navigation, and content requirements."
+          des=""
         >
           <CanvasRevealEffect
             animationSpeed={5.1}
@@ -29,9 +27,7 @@ const Approach = () => {
         <Card
           title="Development & Progress Update"
           icon={<AceternityIcon order="Phase 2" />}
-          des="Once we agree on the plan, I cue my lofi playlist and dive into
-          coding. From initial sketches to polished code, I keep you updated
-          every step of the way."
+          des=""
         >
           <CanvasRevealEffect
             animationSpeed={3}
@@ -51,9 +47,7 @@ const Approach = () => {
         <Card
           title="Development & Launch"
           icon={<AceternityIcon order="Phase 3" />}
-          des="This is where the magic happens! Based on the approved design, 
-          I'll translate everything into functional code, building your website
-          from the ground up."
+          des=""
         >
           <CanvasRevealEffect
             animationSpeed={3}
